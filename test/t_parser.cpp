eadfde0718a8910d9e2a45d8ff2a2fb8f7a1ed0d
Tomas Bruna <bruna.tomas@gmail.com>
2019-06-26 16:04:58 -0400

    Test for min exon score

--- test/t_parser.cpp
+++ test/t_parser.cpp
@@ -16,19 +16,28 @@ int returnDiff(string expected, string result) {
 TEST_CASE("Test whole program with different settings") {
     Parser fileParser;
     fileParser.setWindowLegth(10);
-    fileParser.setMinExonScore(25);
+
     fileParser.printAllSites();
     string input = PATH + "/test_files/test_1.ali";
     string output = PATH + "/test_files/test_result";
     ScoreMatrix * scoreMatrix = new ScoreMatrix();
     scoreMatrix->loadFromFile(PATH + "/test_files/blosum62_1.csv");
     Kernel * triangularkernel = new TriangularKernel();
+    fileParser.setScoringMatrix(scoreMatrix);
+    fileParser.setKernel(triangularkernel);
 
-    SECTION("BLOSUM62 score") {
-        fileParser.setScoringMatrix(scoreMatrix);
-        fileParser.setKernel(triangularkernel);
+
+    SECTION("All") {
+        fileParser.setMinExonScore(-999999);
         fileParser.parse(input, output);
         int result = returnDiff("test_parser.gff", output);
+        CHECK(result == 0);
+    }
+
+    SECTION("Exon filter 25") {
+        fileParser.setMinExonScore(25);
+        fileParser.parse(input, output);
+        int result = returnDiff("test_parser_eScore_25.gff", output);
         CHECK(result == 0);
     }
 
